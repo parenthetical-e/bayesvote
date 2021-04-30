a08f74cdbab5b03eb7f3def5d9cf6da4389df953
Erik Peterson <Erik.Exists@gmail.com>
2021-04-30 14:57:50 -0700

    Add initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bdcode',
+      version='0.0.1',
+      description="A bayesian decoding library, for time series.",
+      url='',
+      author='Erik J. Peterson',
+      author_email='erik.exists@gmail.com',
+      license='MIT',
+      packages=['bdcode'],
+      zip_safe=False)
